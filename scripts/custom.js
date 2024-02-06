5348532818f7fc268132c981c66839ad22c06003
Alex Moshak <alexm@ensemble.com>
2024-02-05 21:08:01 -0800

    fix featured projects

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,17 +1,21 @@
 export function decorateTitleSection(main) {
-  const list = main.querySelector('#trimark-empowers-you-every-step-of-the-way').nextElementSibling;
-  list.classList.add('title-list');
-  if (list) {
-    list.querySelectorAll('li > span > img').forEach((el) => {
-      el.src = '/icons/diamond.png';
-    });
+  if (main.querySelector('#trimark-empowers-you-every-step-of-the-way')) {
+    const list = main.querySelector('#trimark-empowers-you-every-step-of-the-way').nextElementSibling;
+    list.classList.add('title-list');
+    if (list) {
+      list.querySelectorAll('li > span > img').forEach((el) => {
+        el.src = '/icons/diamond.png';
+      });
+    }
   }
 }
 
 export function decorateBackgroundSection(main) {
   const backgroundSection = main.querySelector('.section.infoblock-container');
-  const backgroundUrl = backgroundSection.dataset.background;
+  if (backgroundSection) {
+    const backgroundUrl = backgroundSection.dataset.background;
 
-  backgroundSection.style.backgroundImage = `url(${backgroundUrl})`;
-  backgroundSection.style.backgroundSize = 'cover';
+    backgroundSection.style.backgroundImage = `url(${backgroundUrl})`;
+    backgroundSection.style.backgroundSize = 'cover';
+  }
 }
